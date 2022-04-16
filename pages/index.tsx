a7e3b6150a6dacb4d7d217c1aa336a3bd702a72f
Tahiel Pedevilla <tahielpedevilladedonato@gmail.com>
2022-04-16 01:02:20 -0300

    Initial commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,85 +2,79 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 
-const Home: NextPage = () => {
+interface CurrencyResponse {
+  fecha: string
+  compra: number
+  venta: number
+}
+
+type Props = {
+  currency: CurrencyResponse
+}
+
+const Home: NextPage<Props> = ({ currency }) => {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center py-2">
+    <div className="flex min-h-screen flex-col items-center justify-center bg-black py-2">
       <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
+        <title>Cotización Dólar Blue</title>
+        <meta content="Tahiel Pedevilla" name="copyright" />
+        <meta content="tahiel" name="author" />
+        <meta
+          name="description"
+          content="Portal para conocer los valores del dólar BLUE con la última cotización"
+        />
+        <meta
+          name="keywords"
+          content="dolar blue, dolar blue cotizacion, dolar blue cotizaciones, dolar blue hoy"
+        />
+        <link rel="icon" href="/favicon.svg" />
       </Head>
 
-      <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          Welcome to{' '}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
-          </a>
+      <main className="flex w-full flex-1 flex-col items-center justify-center bg-black px-20 font-mono text-white">
+        <h1 className="hidden text-3xl font-bold md:block">
+          COTIZACIÓN DÓLAR BLUE
         </h1>
-
-        <p className="mt-3 text-2xl">
-          Get started by editing{' '}
-          <code className="rounded-md bg-gray-100 p-3 font-mono text-lg">
-            pages/index.tsx
-          </code>
-        </p>
-
-        <div className="mt-6 flex max-w-4xl flex-wrap items-center justify-around sm:w-full">
-          <a
-            href="https://nextjs.org/docs"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Discover and deploy boilerplate example Next.js projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+        <h1 className="block text-3xl font-bold md:hidden">
+          COTIZACIÓN <br /> DÓLAR BLUE
+        </h1>
+        <div className="mt-4 flex flex-col items-center justify-center">
+          <article className="flex flex-row items-center justify-between gap-4 rounded bg-green-500 px-4 py-1">
+            <p className="uppercase">Compra</p>
+            <Image src="/arrow-right.svg" width={24} height={24} />
+            <p>${currency.compra}</p>
+          </article>
+          <article className="mt-2 flex flex-row items-center justify-between gap-4 rounded bg-red-500 px-4 py-1">
+            <p className="uppercase">Venta</p>
+            <Image src="/arrow-right.svg" width={24} height={24} />
+            <p>${currency.venta}</p>
+          </article>
+          <span className="mt-8 rounded bg-blue-500 px-2 py-1 text-xs">
+            Última actualización: {currency.fecha}
+          </span>
         </div>
       </main>
-
-      <footer className="flex h-24 w-full items-center justify-center border-t">
-        <a
-          className="flex items-center justify-center gap-2"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-        </a>
-      </footer>
     </div>
   )
 }
 
 export default Home
+
+export const getStaticProps = async () => {
+  const response = await fetch(
+    'https://api-dolar-argentina.herokuapp.com/api/dolarblue'
+  )
+
+  if (!response.ok) throw new Error(`Error! status: ${response.status}`)
+
+  if (response.status === 200) {
+    const currency: CurrencyResponse = await response.json()
+
+    return {
+      props: {
+        currency: currency,
+      },
+
+      revalidate: 30,
+    }
+  }
+}
